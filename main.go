c4d74485515f7e6e69cdbdbf1bb4dba7cf79a8f7
Anton Sukhonosenko <anton.sukhonosenko@jetbrains.com>
2020-04-09 17:23:51 +0300

    root

---
+++ main.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+	"log"
+	"net"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+)
+
+type AppHandler struct{}
+
+func (h AppHandler) ServeHTTP(resp http.ResponseWriter, req *http.Request) {
+	if req.Method == "POST" && req.URL.Path == "/convert" {
+		if err := ProcessConvertRequest(resp, req); err != nil {
+			Error.Printf(err.Error())
+			resp.WriteHeader(500)
+		}
+	} else {
+		resp.WriteHeader(404)
+	}
+}
+
+func app() error {
+	listener, err := net.Listen("tcp", "0.0.0.0:8080")
+	if err != nil {
+		return err
+	}
+
+	Info.Printf("Starting server on %s...", listener.Addr().String())
+
+	server := &http.Server{
+		Handler: AppHandler{},
+	}
+
+	go func() {
+		err = server.Serve(listener)
+		if err != nil {
+			Error.Printf(err.Error())
+		}
+	}()
+
+	stop := make(chan os.Signal, 1)
+	signal.Notify(stop, syscall.SIGINT, syscall.SIGTERM)
+
+	select {
+	case <-stop:
+	}
+
+	return nil
+}
+
+func main() {
+	if err := app(); err != nil {
+		log.Fatal(err)
+	}
+}
