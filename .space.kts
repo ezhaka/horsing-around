563fb023eb9c46d159e2634b43e52e8b5b4f67ea
Mabel Pines <mabel.pines@gravity.falls.com>
2020-09-21 14:13:17 +0300

    example shell script

--- .space.kts
+++ .space.kts
@@ -1,10 +1,17 @@
+
 /**
 * JetBrains Space Automation
 * This Kotlin-script file lets you automate build activities
 * For more info, refer to https://www.jetbrains.com/help/space/automation.html
 */
 
-job("Hello World!") {
-    container("hello-world")
+job("Example shell script") {
+    container("ubuntu") {
+        shellScript {
+            content = """
+                echo Hello
+                echo World!
+            """
+        }
+    }
 }
-
